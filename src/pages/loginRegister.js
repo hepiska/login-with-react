75edf520081df396e50a60305e186afe26597c96
hepiska <ego1403@gmail.com>
2019-08-26 00:51:25 +0700

    handle input v1-done

--- src/pages/loginRegister.js
+++ src/pages/loginRegister.js
@@ -5,12 +5,32 @@ import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
 
 class LoginRegisterPage extends React.Component {
   state = {
-    input: {
+    input: {}
+  }
 
-    }
+  fields = [{
+    name: 'email',
+    icon: 'user',
+    iconPosition: 'left',
+    placeholder: 'E-mail address',
+  },
+  {
+    name: 'password',
+    icon: 'lock',
+    iconPosition: 'left',
+    placeholder: 'Password',
+    type: 'password'
+  }]
+
+  _onFieldChange = (e, data) => {
+    const { input } = this.state
+    const newInput = { ...input }
+    newInput[data.name] = data.value
+    this.setState({ input: newInput })
   }
 
   render() {
+    const { input } = this.state
     return (
       <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -19,15 +39,9 @@ class LoginRegisterPage extends React.Component {
           </Header>
           <Form size='large'>
             <Segment stacked>
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-              <Form.Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
-              />
-
+              {this.fields.map((field) => (
+                <Form.Input fluid {...field} value={input[field.name]} onChange={this._onFieldChange} />
+              ))}
               <Button color='teal' fluid size='large'>
                 Login
               </Button>
